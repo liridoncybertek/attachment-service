7c60e668da949ef434050fb0ec6123b241a88727
Liridon Morina <liridon@cyberktechschool.com>
2021-03-15 11:18:40 +0100

    Add plain attachment service

---
+++ src/test/java/com/cybertek/attachmentservice/AttachmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybertek.attachmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
